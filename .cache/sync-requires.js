a5b44fd4ab011670d9b18690803f6dc2dd90868e
houacine <samira@houacines-MacBook-Pro.local>
2021-05-12 16:57:54 +0200

    site WordPress with react and gatsby

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,12 +4,12 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/samira/simple-life/.cache/dev-404-page.js")),
-  "component---src-pages-404-js": preferDefault(require("/Users/samira/simple-life/src/pages/404.js")),
-  "component---src-templates-blog-post-archive-js": preferDefault(require("/Users/samira/simple-life/src/templates/blog-post-archive.js")),
-  "component---src-templates-blog-post-js": preferDefault(require("/Users/samira/simple-life/src/templates/blog-post.js")),
-  "component---src-templates-category-js": preferDefault(require("/Users/samira/simple-life/src/templates/category.js")),
-  "component---src-templates-page-js": preferDefault(require("/Users/samira/simple-life/src/templates/page.js")),
-  "component---src-templates-tag-js": preferDefault(require("/Users/samira/simple-life/src/templates/tag.js"))
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/samira/Documents/simple-life/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/samira/Documents/simple-life/src/pages/404.js")),
+  "component---src-templates-blog-post-archive-js": preferDefault(require("/Users/samira/Documents/simple-life/src/templates/blog-post-archive.js")),
+  "component---src-templates-blog-post-js": preferDefault(require("/Users/samira/Documents/simple-life/src/templates/blog-post.js")),
+  "component---src-templates-category-js": preferDefault(require("/Users/samira/Documents/simple-life/src/templates/category.js")),
+  "component---src-templates-page-js": preferDefault(require("/Users/samira/Documents/simple-life/src/templates/page.js")),
+  "component---src-templates-tag-js": preferDefault(require("/Users/samira/Documents/simple-life/src/templates/tag.js"))
 }
 
--- public/render-page.js
+++ public/render-page.js
@@ -13789,24 +13789,24 @@ module.exports = JSON.parse('{"data":{"wp":{"generalSettings":{"title":"Simple L
 /***/ }),
 
 /***/ "react-dom/server":
-/*!*****************************************************************************!*\
-  !*** external "/Users/samira/simple-life/node_modules/react-dom/server.js" ***!
-  \*****************************************************************************/
+/*!***************************************************************************************!*\
+  !*** external "/Users/samira/Documents/simple-life/node_modules/react-dom/server.js" ***!
+  \***************************************************************************************/
 /***/ ((module) => {
 
 "use strict";
-module.exports = require("/Users/samira/simple-life/node_modules/react-dom/server.js");;
+module.exports = require("/Users/samira/Documents/simple-life/node_modules/react-dom/server.js");;
 
 /***/ }),
 
 /***/ "react":
-/*!************************************************************************!*\
-  !*** external "/Users/samira/simple-life/node_modules/react/index.js" ***!
-  \************************************************************************/
+/*!**********************************************************************************!*\
+  !*** external "/Users/samira/Documents/simple-life/node_modules/react/index.js" ***!
+  \**********************************************************************************/
 /***/ ((module) => {
 
 "use strict";
-module.exports = require("/Users/samira/simple-life/node_modules/react/index.js");;
+module.exports = require("/Users/samira/Documents/simple-life/node_modules/react/index.js");;
 
 /***/ }),
 
